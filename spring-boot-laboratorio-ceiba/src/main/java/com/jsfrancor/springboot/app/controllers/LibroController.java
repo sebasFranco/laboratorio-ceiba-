b7dfb9738d51f4a909c132191b566f030a02b759
Sebastian Franco <jsfrancor2@gmail.com>
2020-01-25 16:02:20 -0500

    prueba integracion libros

--- spring-boot-laboratorio-ceiba/src/main/java/com/jsfrancor/springboot/app/controllers/LibroController.java
+++ spring-boot-laboratorio-ceiba/src/main/java/com/jsfrancor/springboot/app/controllers/LibroController.java
@@ -4,6 +4,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,12 +17,13 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.jsfrancor.springboot.app.models.entity.Libro;
 import com.jsfrancor.springboot.app.service.ILibroService;
 
-@Component
+@Controller
 @SessionAttributes("libro")
 public class LibroController {
 	
 	@Autowired
 	private ILibroService libroService;
+	
 	
 	@GetMapping("/listar")
 	public String listar(Model model) {
