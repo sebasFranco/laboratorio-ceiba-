92a2f6dedc8ee1773b6f1126e0b6b5d63d20996f
iXGuardian <samirrhusein@gmail.com>
2020-01-26 05:47:37 -0500

    Test_Clase_Libro
    Creamos y modificamos los valores de la clase libro

--- spring-boot-laboratorio-ceiba/src/test/java/com/jsfrancor/springboot/app/unitario/PrestamoTest.java
+++ spring-boot-laboratorio-ceiba/src/test/java/com/jsfrancor/springboot/app/unitario/PrestamoTest.java
@@ -1,5 +1,24 @@
 package com.jsfrancor.springboot.app.unitario;
 
-public class PrestamoTest {
 
+import static org.mockito.Mockito.*;
+
+
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+import org.mockito.stubbing.Answer;
+
+public class PrestamoTest {
+	
+	@Rule public MockitoRule mockitoRule = MockitoJUnit.rule();
+	
+	@Mock
+	private Prestamo prestamo;
+	
+	
+	
 }
+		
